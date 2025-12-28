8ef507101e180ff97ad4f319d91ab0e6c9d7b552
marco.mengelkoch <Mmengelkoch@gmx.de>
2025-12-28 16:41:09 +0100

    fix failing test

--- tests/memory_test.rs
+++ tests/memory_test.rs
@@ -6,7 +6,7 @@ use crate::integration::common::format_pretty;
 mod integration;
 
 // run in release:
-// cargo test --package mq-bridge --test memory_test --features integration-test --release -- test_memory_to_memory_pipeline --exact --nocapture
+// cargo test --package mq-bridge --test memory_test --release -- test_memory_to_memory_pipeline --exact --nocapture --ignored
 
 #[tokio::test]
 #[ignore] // This is a performance test, run it explicitly
@@ -23,7 +23,7 @@ async fn test_memory_to_memory_pipeline() {
     let messages_to_send = integration::common::generate_test_messages(num_messages);
 
     let input = Endpoint::new_memory("mem-in", 200);
-    let output = Endpoint::new_memory("mem-out", 200);
+    let output = Endpoint::new_memory("mem-out", num_messages);
     let route = Route::new(input, output);
     let in_channel = route.input.channel().unwrap();
     let out_channel = route.output.channel().unwrap();
